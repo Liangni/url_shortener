9bd8792ed0c3d7486f1e3f942341f07110ea020f
Liangni <light0104@gmail.com>
2021-10-19 17:13:00 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Url = require('./models/url')
 const Utility = require('./utility')
+const routes =require('./routes')
 
 const app = express()
 const PORT = 3000
-const domain = `http://localhost:${PORT}/`
 
 mongoose.connect('mongodb://localhost/url-shortener-list')
 
@@ -25,59 +25,7 @@ app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-// 設定產生短網址
-app.post('/urls', (req, res) =>{
-  let input = req.body.input.trim()
-  let shortenedUrl = ''
-  
-  // 例外處理: 去除使用者輸入網址結尾的"/"，統一相同url儲存格式
-  const lastIndex = input.length - 1
-  if (input[lastIndex] === '/') {
-    input = input.slice(0, -1)
-  }
-
-  return Url.findOne({ url:input, $options:"i" })
-    .lean()
-    .then( url => {
-      
-      // 例外處理: 輸入相同網址時，產生一樣的縮址
-      if (url) {
-        shortenedUrl = domain + url.suffix
-      }
-      
-      // 產生短網址
-      else  {
-          const suffix = Utility.suffixGenerator()
-          Url.create({ url: input, suffix: suffix })
-          shortenedUrl = domain + suffix
-        }
-    })
-    .then(() => {
-      res.render('index', { shortenedUrl })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
-
-// 設定轉址路由
-app.get('/:suffix', (req, res) => {
-  const suffix = req.params.suffix
-  return Url.findOne({ suffix })
-    .lean()
-    .then(url => {
-      res.redirect(url.url)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
+app.use(routes)
 
 
 app.listen(PORT, () => {
